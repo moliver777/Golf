fa772afc53bd908d1c6e0bd8ead7c38407fd0089
Alex Wadeson <awadeson@i-neda.com>
2014-06-10 17:49:42 +0100

    added admin.js to pipeline

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module Golf
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.precompile += ["admin.css", "leaderboard.css", "admin.js"]
+    config.assets.precompile += ["admin.css", "leaderboard.css"]
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
